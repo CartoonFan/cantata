cfa4c28b87f19d426690fe3e4679f1461c392dcb
Restyled.io <commits@restyled.io>
2020-06-12 17:32:07 +0000

    Restyled by black

--- icons/yaru/render-bitmaps.py
+++ icons/yaru/render-bitmaps.py
@@ -27,8 +27,8 @@ import xml.sax
 import subprocess
 import argparse
 
-INKSCAPE = '/usr/bin/inkscape'
-OPTIPNG = '/usr/bin/optipng'
+INKSCAPE = "/usr/bin/inkscape"
+OPTIPNG = "/usr/bin/optipng"
 
 # DPI multipliers to render at
 DPIS = [1, 2]
@@ -37,30 +37,33 @@ inkscape_process = None
 
 
 def main(SRC):
-
     def optimize_png(png_file):
         if os.path.exists(OPTIPNG):
-            process = subprocess.Popen([OPTIPNG, '-quiet', '-o7', png_file])
+            process = subprocess.Popen([OPTIPNG, "-quiet", "-o7", png_file])
             process.wait()
 
     def wait_for_prompt(process, command=None):
         if command is not None:
-            process.stdin.write((command+'\n').encode('utf-8'))
+            process.stdin.write((command + "\n").encode("utf-8"))
 
         # This is kinda ugly ...
         # Wait for just a '>', or '\n>' if some other char appearead first
         output = process.stdout.read(1)
-        if output == b'>':
+        if output == b">":
             return
 
         output += process.stdout.read(1)
-        while output != b'\n>':
+        while output != b"\n>":
             output += process.stdout.read(1)
             output = output[1:]
 
     def start_inkscape():
         process = subprocess.Popen(
-            [INKSCAPE, '--shell'], bufsize=0, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+            [INKSCAPE, "--shell"],
+            bufsize=0,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+        )
         wait_for_prompt(process)
         return process
 
@@ -69,11 +72,8 @@ def main(SRC):
         if inkscape_process is None:
             inkscape_process = start_inkscape()
 
-        cmd = [icon_file,
-               '--export-dpi', str(dpi),
-               '-i', rect,
-               '-e', output_file]
-        wait_for_prompt(inkscape_process, ' '.join(cmd))
+        cmd = [icon_file, "--export-dpi", str(dpi), "-i", rect, "-e", output_file]
+        wait_for_prompt(inkscape_process, " ".join(cmd))
         optimize_png(output_file)
 
     class ContentHandler(xml.sax.ContentHandler):
@@ -103,8 +103,13 @@ def main(SRC):
                     self.inside.append(self.SVG)
                     return
             elif self.inside[-1] == self.SVG:
-                if (name == "g" and ('inkscape:groupmode' in attrs) and ('inkscape:label' in attrs)
-                        and attrs['inkscape:groupmode'] == 'layer' and attrs['inkscape:label'].startswith('Baseplate')):
+                if (
+                    name == "g"
+                    and ("inkscape:groupmode" in attrs)
+                    and ("inkscape:label" in attrs)
+                    and attrs["inkscape:groupmode"] == "layer"
+                    and attrs["inkscape:label"].startswith("Baseplate")
+                ):
                     self.stack.append(self.LAYER)
                     self.inside.append(self.LAYER)
                     self.context = None
@@ -112,16 +117,24 @@ def main(SRC):
                     self.rects = []
                     return
             elif self.inside[-1] == self.LAYER:
-                if name == "text" and ('inkscape:label' in attrs) and attrs['inkscape:label'] == 'context':
+                if (
+                    name == "text"
+                    and ("inkscape:label" in attrs)
+                    and attrs["inkscape:label"] == "context"
+                ):
                     self.stack.append(self.TEXT)
                     self.inside.append(self.TEXT)
-                    self.text = 'context'
+                    self.text = "context"
                     self.chars = ""
                     return
-                elif name == "text" and ('inkscape:label' in attrs) and attrs['inkscape:label'] == 'icon-name':
+                elif (
+                    name == "text"
+                    and ("inkscape:label" in attrs)
+                    and attrs["inkscape:label"] == "icon-name"
+                ):
                     self.stack.append(self.TEXT)
                     self.inside.append(self.TEXT)
-                    self.text = 'icon-name'
+                    self.text = "icon-name"
                     self.chars = ""
                     return
                 elif name == "rect":
@@ -135,10 +148,10 @@ def main(SRC):
                 self.inside.pop()
 
             if stacked == self.TEXT and self.text is not None:
-                assert self.text in ['context', 'icon-name']
-                if self.text == 'context':
+                assert self.text in ["context", "icon-name"]
+                if self.text == "context":
                     self.context = self.chars
-                elif self.text == 'icon-name':
+                elif self.text == "icon-name":
                     self.icon_name = self.chars
                 self.text = None
             elif stacked == self.LAYER:
@@ -151,45 +164,44 @@ def main(SRC):
                 print(self.context, self.icon_name)
                 for rect in self.rects:
                     for dpi_factor in DPIS:
-                        width = rect['width']
-                        height = rect['height']
-                        id = rect['id']
+                        width = rect["width"]
+                        height = rect["height"]
+                        id = rect["id"]
                         dpi = 96 * dpi_factor
 
                         size_str = "%sx%s" % (width, height)
                         if dpi_factor != 1:
                             size_str += "@%sx" % dpi_factor
 
-                        outfile = self.icon_name+'-'+size_str+'.png'
+                        outfile = self.icon_name + "-" + size_str + ".png"
                         # Do a time based check!
                         if self.force or not os.path.exists(outfile):
                             inkscape_render_rect(self.path, id, dpi, outfile)
-                            sys.stdout.write('.')
+                            sys.stdout.write(".")
                         else:
                             stat_in = os.stat(self.path)
                             stat_out = os.stat(outfile)
                             if stat_in.st_mtime > stat_out.st_mtime:
-                                inkscape_render_rect(
-                                    self.path, id, dpi, outfile)
-                                sys.stdout.write('.')
+                                inkscape_render_rect(self.path, id, dpi, outfile)
+                                sys.stdout.write(".")
                             else:
-                                sys.stdout.write('-')
+                                sys.stdout.write("-")
                         sys.stdout.flush()
-                sys.stdout.write('\n')
+                sys.stdout.write("\n")
                 sys.stdout.flush()
 
         def characters(self, chars):
             self.chars += chars.strip()
 
-    print('')
-    print('Rendering from SVGs in', SRC)
-    print('')
+    print("")
+    print("Rendering from SVGs in", SRC)
+    print("")
     for file in os.listdir(SRC):
-        if file[-4:] == '.svg':
+        if file[-4:] == ".svg":
             file = os.path.join(SRC, file)
             handler = ContentHandler(file)
             xml.sax.parse(open(file), handler)
-    print('')
+    print("")
 
 
-main('.')
+main(".")
